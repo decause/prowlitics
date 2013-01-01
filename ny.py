91b6584fc2f8ac6e7a45ed5ce235a79933ab2220
RemyD <remyd@civx.us>
2013-01-01 13:17:16 -0500

    Initial version of NY script

---
+++ ny.py
@@ -0,0 +1,56 @@
+from sqlalchemy import create_engine
+from knowledge.model import init_model, metadata, Entity, DBSession
+from kitchen.text.converters import to_unicode
+
+import sunlight
+engine = create_engine('sqlite:///knowledge.db')
+init_model(engine)
+metadata.create_all(engine)
+
+# Query all the Entities out of knowledge
+knowledge_query = DBSession.query(Entity).all()
+for entity in knowledge_query:
+    print entity, entity.facts.values()
+
+ny_legs = sunlight.openstates.legislators(state='ny')
+
+
+def inject_test_knowledge():
+    monster = Entity(u'Monster')
+    fairy = Entity(u'Fairy')
+    rjbean = Entity(u'rjbean')
+    monster[u'color'] = u'Green'
+    monster[u'name'] = u'Lotharrr'
+    fairy[u'flies'] = True
+    fairy[u'name'] = u'Bell'
+    rjbean[u'name'] = u'ralph'
+    rjbean[u'flies'] = False
+    rjbean[u'hacks'] = True
+
+    DBSession.add(monster)
+    DBSession.add(fairy)
+    DBSession.add(rjbean)
+    DBSession.commit()
+
+
+def inject_knowledge():
+
+    knowledge = DBSession
+    for leg in ny_legs:
+        character = Entity(u'%s' % leg['full_name'])
+        character[u'name'] = (u'%s' % leg['full_name'])
+        for key, value in leg.items():
+            character[key] = to_unicode(value)
+        knowledge.add(character)
+        knowledge.commit()
+
+inject_knowledge()
+
+
+# Query all the Entities out of knowledge
+def the_facts():
+    knowledge_query = DBSession.query(Entity).all()
+    for entity in knowledge_query:
+        print entity, entity.facts.values
+
+the_facts()

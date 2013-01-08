bc66fca2e0f713a53a31d26d93154caea6e6e0be
RemyD <remyd@civx.us>
2013-01-07 19:25:01 -0500

    Added comments, re-narrowed to NY only

--- ny.py
+++ ny.py
@@ -1,16 +1,12 @@
 from sqlalchemy import create_engine
 from knowledge.model import init_model, metadata, Entity, DBSession
-from kitchen.text.converters import to_unicode, to_bytes
+from kitchen.text.converters import to_bytes, to_unicode
 
 import sunlight
+
 engine = create_engine('sqlite:///knowledge.db')
 init_model(engine)
 metadata.create_all(engine)
-
-# Query all the Entities out of knowledge
-knowledge_query = DBSession.query(Entity).all()
-for entity in knowledge_query:
-    print to_bytes(entity), to_bytes(entity.facts.values())
 
 ny_legs = sunlight.openstates.legislators(state='ny')
 
@@ -34,13 +30,23 @@ def inject_test_knowledge():
 
 
 def inject_knowledge():
-
+    #SunlightAPI returns nicely Unicoded data, but not all APIs will...
+    #Don't forget to use to_unicode()
     knowledge = DBSession
     for leg in ny_legs:
-        character = Entity(u'%s' % to_unicode(leg['full_name']))
-        character[u'name'] = (u'%s' % to_unicode(leg['full_name']))
+        print leg['full_name']
+        #Check if entity is already in DB, and if so continue
+        #This is good for not duplicating entries, however
+        #This will skip updates to the entity :(
+        #This is why Sunlight included an "updated_at" field :)
+        #TODO: Change check to "updated_at" instead of full_name
+        if Entity.by_name(leg['full_name']):
+            continue
+            character = Entity.by_name(leg['full_name'])
+        else:
+            character = Entity(leg['full_name'])
         for key, value in leg.items():
-            character[to_unicode(key)] = to_unicode(value)
+            character[key] = to_unicode(value)
         knowledge.add(character)
         knowledge.commit()
 
@@ -52,5 +58,6 @@ def the_facts():
     knowledge_query = DBSession.query(Entity).all()
     for entity in knowledge_query:
         print to_bytes(entity), to_bytes(entity.facts.values())
+
 
 the_facts()
